2cb4f9a24e9ac28d41dd94783fb05dd9e7f99bf7
dominikapara <dominika.para16@gmail.com>
2021-03-21 22:50:45 +0100

    canCreateCarInstance - Pass

--- src/CarTests.java
+++ src/CarTests.java
@@ -1,10 +1,10 @@
 import org.junit.jupiter.api.Test;
 
-
 public class CarTests {
 
     @Test
     void canCreateCarInstance() {
-        Car car = new Car();
+        new Car("Blue", "Honda", 6.5f, 30);
     }
+
 }
