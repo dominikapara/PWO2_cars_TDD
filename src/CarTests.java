59f815ddd0b7391fde2d9c43aea35a4b57d8042d
dominikapara <dominika.para16@gmail.com>
2021-03-21 23:37:38 +0100

    carInstanceCantHaveFuelLevelBiggerThanTankCapacity - Fail

--- src/CarTests.java
+++ src/CarTests.java
@@ -8,12 +8,27 @@ public class CarTests {
         String make = "Honda";
         float fuelConsumption = 6.5f;
         int tankCapacity = 30;
+        float fuelLevel = 17.5f;
+        int odometer = 170000;
+        float dailyOdometer = 180;
 
-        Car car = new Car(color, make, fuelConsumption, tankCapacity);
+        Car car = new Car(color, make, fuelConsumption, tankCapacity, fuelLevel, odometer, dailyOdometer);
         assert car.getColor().equals(color);
         assert car.getMake().equals(make);
         assert car.getFuelConsumption() == fuelConsumption;
         assert car.getTankCapacity() == tankCapacity;
+        assert car.getFuelLevel() == fuelLevel;
+        assert car.getOdometer() == odometer;
+        assert car.getDailyOdometer() == dailyOdometer;
     }
 
+    @Test
+    void carInstanceCantHaveFuelLevelBiggerThanTankCapacity() {
+        int tankCapacity = 20;
+        float fuelLevel = 30;
+
+        Assertions.assertThrows(FuelLevelBiggerThanTankCapacityException.class,
+                () -> new Car("color", "make", 1f, tankCapacity, fuelLevel, 1, 1)
+        );
+    }
 }
